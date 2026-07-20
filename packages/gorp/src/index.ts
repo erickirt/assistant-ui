84d98168ebf4f3919086883fcaccbd68cba1797f
Simon Farshid <simon@assistant-ui.com>
2026-07-20 10:44:16 -0400

    chore: remove @assistant-ui/gorp (migrated into assistant-stream assistant-transport) (#5071)

--- packages/gorp/src/index.ts
+++
@@ -1,8 +0,0 @@
-export { appendText } from "./Gorp";
-export type { GorpMessage } from "./Gorp";
-export { GorpClient } from "./GorpClient";
-export { GorpServer } from "./GorpServer";
-export { GorpRelay } from "./GorpRelay";
-export type { RelaySerializedState } from "./GorpRelay";
-export { GorpSessions } from "./GorpSessions";
-export type { GorpSessionsState } from "./GorpSessions";
